62220f4740ccc7db35cf5afa04d0820c7ef55d22
Lucas Emanuel Resck <41991486+lucasresck@users.noreply.github.com>
2022-07-22 22:55:49 -0300

    Improve `find_stories_on_video` function
    Create data path if it does not exist, search for Portuguese
    transcription only, create `check_historia` function, and save print to
    file with `my_print`

--- py/find_stories.py
+++ py/find_stories.py
@@ -1,25 +1,47 @@
+from pathlib import Path
 from youtube_transcript_api import YouTubeTranscriptApi
 
+def my_print(text, file, end="\n"):
+    print(text, end=end)
+    print(text, file=file, end=end)
+
 def print_transcription(transcriptions, transcription, i, video_id):
+    f = open(f"{video_id}.txt", 'a')
     for j in range(max([i - 3, 0]), min([i + 3, len(transcriptions)])):
         if j == i:
-            print(">> ", end="")
-        print(transcriptions[j]['text'])
-    print()
+            my_print(">> ", f, end="")
+        my_print(transcriptions[j]['text'], f)
+    my_print("", f)
 
-    print(f"Time: {int(transcription['start']//60)}:{int(transcription['start']%60)}")
-    print(f"Duration: {int(transcription['duration'])} seconds")
-    print(f"Link: https://www.youtube.com/watch?v={video_id}&t={int(transcription['start'])}")
-    print()
-    print()
+    my_print(f"Time: {int(transcription['start']//60)}:{int(transcription['start']%60)}", f)
+    my_print(f"Duration: {int(transcription['duration'])} seconds", f)
+    my_print(f"Link: https://www.youtube.com/watch?v={video_id}&t={int(transcription['start'])}", f)
+    my_print("", f)
+    my_print("", f)
+    f.close()
 
-def find_stories_on_video(video_id):
+def check_historia(text):
+    text = text.lower()
+    return "história" in text or "historia" in text
+
+def find_stories_on_video(video_id, data_path="../data/video_transcriptions"):
     """Find Elon Musk miraculous stories on a video.
 
     Args:
         video_id (str): YouTube video id.
     """
-    transcriptions = YouTubeTranscriptApi.get_transcript(video_id, languages=['pt'])
+    # Create data_path if it doesn't exist
+    data_path = Path(data_path)
+    data_path.mkdir(parents=True, exist_ok=True)
+
+    transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
+    transcript_list = [transcript for transcript in transcript_list if transcript.language_code == 'pt']
+    if len(transcript_list) == 0:
+        print(f"No transcript found for video {video_id}")
+        return
+    assert len(transcript_list) == 1, "More than one transcript found for video"
+
+    transcriptions = transcript_list[0].fetch()
     for i, transcription in enumerate(transcriptions):
-        if "história" in transcription['text'].lower():
+        if check_historia(transcription['text']):
             print_transcription(transcriptions, transcription, i, video_id)
